0f0644dfea8939f394d88c9521ca1a4999b8dbc6
Steffen Liebergeld <perl@gmx.org>
2017-10-09 21:12:14 +0200

    init.el: cleanup and strenghtening

--- init.el
+++ init.el
@@ -1,25 +1,32 @@
+;; keep this config clean
+(setq custom-file "~/.emacs.d/custom.el")
+(if (file-readable-p custom-file)
+    (load custom-file))
+
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 ;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-;; ensure repo cache is up1date
-(or (file-exists-p package-user-dir)
-    (package-refresh-contents))
+;; ensure repo cache is up to date (don't to that on slow systems...)
+(if (not (string= system-name "localhost"))
+    (or (file-exists-p package-user-dir)
+        (package-refresh-contents)))
 
 ;; Install use-package
 (defun ensure-package-installed (package)
   "Ensure packages are installed"
-  (if (package-installed-p package)
-      nil
+  (if (not (package-installed-p package))
     (progn
       (package-refresh-contents)
       (package-install package))))
 
 (ensure-package-installed 'use-package)
+(setq use-package-always-ensure t)
 
 (require 'async-bytecomp)
+(async-bytecomp-package-mode 1)
 
 ;; hide startup message
 (setq inhibit-splash-screen t
@@ -64,7 +71,6 @@
 
 (use-package saveplace
   :demand t
-  :ensure t
   :config
   (progn
     (setq save-place-file "~/.emacs.d/saveplace") ;; remember cursor positions of open files
@@ -75,7 +81,7 @@
 ;(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 ;; set c++-mode for files without extension
-(setq default-major-mode 'c++-mode)
+(setq major-mode 'c++-mode)
 
 ;;; esc quits, used for evil-mode
 (defun minibuffer-keyboard-quit ()
@@ -90,34 +96,36 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
     (abort-recursive-edit)))
 
 (use-package evil
-  :ensure t
   :demand t
   :config
   (progn
-    ;; make '*' and '#' search for the whole
-    ;; symbol and not only the word under the cursor, just like in vim
-    (setq evil-symbol-word-search t)
-    (setq evil-shift-width 2)
     ;; enable evil-mode everywhere
     (evil-mode t)
-    (setq evil-emacs-state-cursor '("red" box))
-    (setq evil-normal-state-cursor '("green" box))
-    (setq evil-visual-state-cursor '("orange" box))
-    (setq evil-insert-state-cursor '("red" bar))
-    (setq evil-replace-state-cursor '("red" bar))
-    (setq evil-operator-state-cursor '("red" hollow))
-    ;; toggle evil-mode with C-z
-    (setq evil-toggle-key "C-z")
+    (setq evil-emacs-state-cursor '("red" box)
+          evil-normal-state-cursor '("green" box)
+          evil-visual-state-cursor '("orange" box)
+          evil-insert-state-cursor '("red" bar)
+          evil-replace-state-cursor '("red" bar)
+          evil-operator-state-cursor '("red" hollow)
+          ;; make '*' and '#' search for the whole
+          ;; symbol and not only the word under the cursor, just like in vim
+          evil-symbol-word-search t
+          evil-shift-width 2
+          ;; toggle evil-mode with C-z
+          evil-toggle-key "C-z"
+          evil-jumper-auto-center t
+          evil-jumper-file  "~/.emacs.d/cache/evil-jumps"
+          evil-jumper-auto-save-interval 3600
+          ;; fix tab in tmux
+          evil-want-C-i-jump nil
+          )
     ;; dont cry when I do :W instead of :w
     (evil-ex-define-cmd "W" 'save-buffer)
     ;; dont quit emacs when I :q
     ;(evil-ex-define-cmd "q" 'kill-buffer)
+    ;; use swiper for searching
     (define-key evil-normal-state-map "/" 'swiper)
     (evil-define-key 'normal org-mode-map (kbd "<tab>") #'org-cycle)
-    (setq  evil-jumper-auto-center t
-           evil-jumper-file  "~/.emacs.d/cache/evil-jumps"
-           evil-jumper-auto-save-interval 3600)
-    (setq evil-want-C-i-jump nil) ;; this fixes TAB in tmux
     (define-key evil-normal-state-map (kbd "*")
       (lambda () (interactive) (swiper (format "\\<%s\\>" (thing-at-point 'symbol)))))
     (define-key evil-normal-state-map (kbd "#")
@@ -134,8 +142,8 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
     ))
 
 (use-package evil-leader
-  :ensure t
   :demand t
+  :disabled
   :config
   (progn
     (global-evil-leader-mode)
@@ -147,16 +155,13 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
       "k" 'kill-buffer)))
 
 (use-package evil-magit
-  :ensure t
   :config
   (setq evil-magit-state 'motion))
 
-(use-package evil-org
-  :ensure t)
+(use-package evil-org)
 
-(use-package evil-escape-mode
-  :if (not (string= system-name "localhost"))
-  :ensure t
+(use-package evil-escape
+  :if (not (string= system-name "localhost")) ; doesn't work on android
   :config
   (progn
     (setq-default evil-escape-key-sequence "kj")
@@ -165,47 +170,40 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 ;;(modify-syntax-entry ?_ "w")
 
-(use-package evil-visualstar
-  :ensure t)
+(use-package evil-visualstar)
 
-(use-package evil-indent-textobject
-  :ensure t)
+(use-package evil-indent-textobject)
 
 (use-package evil-surround
-  :ensure t
   :config
   (global-evil-surround-mode t))
 
 (use-package evil-matchit
-  :ensure t
   :config
   (global-evil-matchit-mode t))
 
 (use-package evil-search-highlight-persist
-  :ensure t
   :config
   (global-evil-search-highlight-persist t))
 
-;; dont use evil in ansi-term
-;; disabled- can temporarily disable it with C-z
+;; disable use evil-mode in ansi-term
 ;(eval-after-load 'evil-vars
 ;  '(evil-set-initial-state 'term-mode 'emacs))
 
+;; disable evil-mode in bongo
 (eval-after-load 'evil-vars
   '(evil-set-initial-state 'bongo-playlist-mode 'emacs))
 
-(global-set-key (kbd "<f2>") 'compile)
-(global-set-key (kbd "<f3>") 'magit-status)
-(global-set-key (kbd "<f4>") 'org-capture)
+;;(global-set-key (kbd "<f2>") 'compile)
+;;(global-set-key (kbd "<f3>") 'magit-status)
+;;(global-set-key (kbd "<f4>") 'org-capture)
+;;(global-set-key (kbd "<f2>") 'bongo-pause/resume)
 (global-set-key (kbd "C-x C-b") 'buffer-menu)
 ;; this disables the strange habit of trying to put emacs in background on C-z
 (global-unset-key (kbd "C-z"))
 
-;;(global-set-key (kbd "<f2>") 'bongo-pause/resume)
-
 ;; org-mode - this is why I am here and not in vim
 (use-package org
-  :ensure t
   :config
   (progn
     (cond
@@ -221,8 +219,9 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
         (setq org-directory "~/kk/org/")))
      ((t)
-      (message
-       "Warning: Cannot properly setup org as this is an unknown host")))
+      (progn
+        (setq org-directory "~/org")
+        (message "Warning: Cannot properly setup org as this is an unknown host"))))
 
     (define-key global-map "\C-cl" 'org-store-link)
     (define-key global-map "\C-ca" 'org-agenda)
@@ -257,12 +256,10 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 ;; powerline
 (use-package powerline
-  :ensure t
   :demand t
   :disabled)
 
 (use-package powerline-evil
-  :ensure t
   :disabled
   :config
   (powerline-evil-vim-color-theme))
@@ -270,22 +267,20 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 ; do not load this on my phone
 (use-package projectile
   :if (not (string= system-name "localhost"))
-  :ensure t
   :config
   (progn
-    (projectile-global-mode t)
+    (projectile-mode t)
     (setq projectile-completion-system 'ivy)))
 
 (use-package terminal-here
-  :ensure t
   :config
   (progn
-    (setq terminal-here-terminal-command (list "/usr/bin/konsole"))
+    (if (file-exists-p "/usr/bin/konsole")
+        (setq terminal-here-terminal-command (list "/usr/bin/konsole")))
     (global-set-key (kbd "C-x t") #'terminal-here-launch)))
 
 ;; XXX: candidate for elimination because ansi-term seems to work great
 (use-package multi-term
-  :ensure t
   :config
   (progn
     ;; allow to send an escape-code in ansi-term
@@ -293,8 +288,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
     ))
 
 ;; show fill-column in prog-modes and org-mode
-(use-package fill-column-indicator
-  :demand t)
+(use-package fill-column-indicator)
 
 (add-hook 'prog-mode-hook (lambda ()
                            (turn-on-auto-fill)
@@ -314,7 +308,6 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
 
 (use-package ivy
-  :ensure t
   :demand t
   :config (progn
             (ivy-mode 1)
@@ -325,7 +318,6 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 (use-package counsel
   :demand t
-  :ensure t
   :config
   (progn
     (global-set-key (kbd "C-c l") 'counsel-locate)
@@ -337,20 +329,19 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
     (global-set-key (kbd "C-c j") 'counsel-git-grep)
     (global-set-key (kbd "C-c k") 'counsel-ag)
     (global-set-key (kbd "C-x l") 'counsel-locate)
-    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+    ;(global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
     (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
     ))
 
 (use-package swiper
   :demand t
-  :ensure t
   :bind ("C-s" . swiper)
   :config
   (progn
     (setq enable-recursive-minibuffers t)
     (global-set-key "\C-s" 'swiper)
     (global-set-key (kbd "C-c C-r") 'ivy-resume)
-    (global-set-key (kbd "<f6>") 'ivy-resume)
+    ;(global-set-key (kbd "<f6>") 'ivy-resume)
     ))
 
 ;; lua-mode required for anything LUA
@@ -365,7 +356,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
     (setq easy-hugo-sshdomain "rpi")
     (setq easy-hugo-root "/var/stepardo.de")
     (setq easy-hugo-previewtime "300")
-    (define-key global-map (kbd "C-c C-e") 'easy-hugo)
+    ;(define-key global-map (kbd "C-c C-e") 'easy-hugo)
     (setq easy-hugo-default-ext ".org")
     ))
 
@@ -387,15 +378,32 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
 
-(load "/usr/share/emacs/site-lisp/clang-format-3.8/clang-format.el")
-(global-set-key [C-M-y] 'clang-format-region)
+;; clang format -> load support if clang-format.el is installed
+;; (on most systems it is not installed...)
+(let ((clang-format-file
+      "/usr/share/emacs/site-lisp/clang-format-3.8/clang-format.el"))
+  (if (file-readable-p clang-format-file)
+      (progn
+        (load clang-format-file)
+        (global-set-key [C-M-y] 'clang-format-region)
+        (message "clang format supported"))
+    (progn
+      (message "clang format not installed -> not loaded")
+      (message "apt-get install -y clang-format-3.8 if you want"))))
 
 (add-to-list 'load-path (concat user-emacs-directory "config"))
 
 ;; (require 'mu4e-conf)
 
-(defvar grep-opts "-rnH --binary-files=without-match --exclude-dir=html --exclude-dir=doc --exclude-dir=.git --exclude-dir=.svn")
-(setq-default grep-command (concat "grep " grep-opts))
+(setq-default grep-command (mapconcat 'identity
+                                      '("grep"
+                                        "-rnH"
+                                        "--binary-files=without-match"
+                                        "--exclude-dir=html"
+                                        "--exclude-dir=doc"
+                                        "--exclude-dir=.git"
+                                        "--exclude-dir=.svn ")
+                                      " "))
 
 ;; directory-local stuff
 (dir-locals-set-class-variables 'l4re-dir
@@ -406,7 +414,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 
 (dir-locals-set-class-variables 'linux-dir
 				'((nil . ((indent-tabs-mode . t)
-					  (tab-width        . 4)
+					  (tab-width        . 8)
 					  (c-default-style  . "linux")))))
 
 (dir-locals-set-directory-class "/home/steffen/kk/git/repo" 'l4re-dir)
@@ -417,19 +425,22 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 	  '(lambda ()
 	     (if buffer-file-name
 		 (set (make-local-variable 'compile-command)
-		      (format "perl generate_scenario.pl %s && perl run_scenario.pl %s" (shell-quote-argument buffer-file-name) (shell-quote-argument buffer-file-name))))))
+		      (format
+                       "perl generate_scenario.pl %s && perl run_scenario.pl %s"
+                       (shell-quote-argument buffer-file-name)
+                       (shell-quote-argument buffer-file-name))))))
 
 ;; enable spell checking for comments
 
 (add-hook 'c-mode-common-hook 'flyspell-prog-mode)
 
-(defun steffen-insert-new-journal-entry ()
-  "Inserts '*** YYYY-MM-DD Tuesday' at beginning of line and a newline"
-  (interactive)
-  (beginning-of-line)
-  (insert (format "*** %s\n" (format-time-string "%Y-%m-%d %A"))))
-
-(global-set-key (kbd "<f5>") 'steffen-insert-new-journal-entry)
+;;(defun steffen-insert-new-journal-entry ()
+;;  "Inserts '*** YYYY-MM-DD Tuesday' at beginning of line and a newline"
+;;  (interactive)
+;;  (beginning-of-line)
+;;  (insert (format "*** %s\n" (format-time-string "%Y-%m-%d %A"))))
+;;
+;;(global-set-key (kbd "<f5>") 'steffen-insert-new-journal-entry)
 
 ;; info mode paths
 ;(add-to-list Info-default-directory-list "/usr/share/info/emacs-24/") ; dpkg -L emacs24-common-non-dfsg
@@ -437,26 +448,3 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
 ;; save sessions
 (desktop-save-mode 1)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(auto-save-file-name-transforms (quote ((".*" "~/.emacs.d/autosaves/\\1" t))))
- '(backup-directory-alist (quote (("." . "~/.emacs.d/backups/"))))
- '(custom-safe-themes
-   (quote
-    ("72c530c9c8f3561b5ab3bf5cda948cd917de23f48d9825b7a781fe1c0d737f2f" "77515a438dc348e9d32310c070bfdddc5605efc83671a159b223e89044e4c4f1" "392f19e7788de27faf128a6f56325123c47205f477da227baf6a6a918f73b5dc" "ff7625ad8aa2615eae96d6b4469fcc7d3d20b2e1ebc63b761a349bebbb9d23cb" "d9e811d5a12dec79289c5bacaecd8ae393d168e9a92a659542c2a9bab6102041" "7bd626fcc9fbfb44186cf3f08b8055d5a15e748d5338e47f9391d459586e20db" "a455366c5cdacebd8adaa99d50e37430b0170326e7640a688e9d9ad406e2edfd" "a11043406c7c4233bfd66498e83600f4109c83420714a2bd0cd131f81cbbacea" "fb09acc5f09e521581487697c75b71414830b1b0a2405c16a9ece41b2ae64222" "1a2cde373eff9ffd5679957c7ecfc6249d353e1ee446d104459e73e924fe0d8a" "5c83b15581cb7274085ba9e486933062652091b389f4080e94e4e9661eaab1aa" "db510eb70cf96e3dbd48f5d24de12b03db30674ea0853f06074d4ccf7403d7d3" "ed92c27d2d086496b232617213a4e4a28110bdc0730a9457edf74f81b782c5cf" "5eb4b22e97ddb2db9ecce7d983fa45eb8367447f151c7e1b033af27820f43760" "05d009b7979e3887c917ef6796978d1c3bbe617e6aa791db38f05be713da0ba0" default)))
- '(evil-toggle-key "C-z")
- '(safe-local-variable-values
-   (quote
-    ((c-default-style . "linux")
-     (c-default-style . "gnu")
-     (compile-command . "make -C . O=/home/steffen/kk/git/build/64")
-     (grep-command . "grep -rnH --binary-files=without-match --exclude-dir=html --exclude-dir=doc --exclude-dir=.git --exclude-dir=.svn")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
