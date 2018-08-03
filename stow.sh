97e3145e6ed94af164926511bbccac1a4a06546d
Steffen Liebergeld <perl@gmx.org>
2018-08-03 14:15:46 +0200

    tmux: add tmux config

--- stow.sh
+++ stow.sh
@@ -2,7 +2,7 @@
 
 CONFIGDIR=`dirname $0`
 
-for project in "zsh" "emacs" "herbstluftwm" "x11" "git";
+for project in "zsh" "emacs" "herbstluftwm" "x11" "git" "tmux";
 do
   stow --dir="$CONFIGDIR" --target="$HOME" -vv "$project";
 done
