e440dbc835dc0214b04d2b644f8ad1c4ef696c36
Steffen Liebergeld <usenet@gmx.eu>
2018-09-01 21:04:48 +0200

    stow.sh: also systemctl enable all services

--- stow.sh
+++ stow.sh
@@ -6,3 +6,9 @@ for project in "zsh" "emacs" "herbstluftwm" "x11" "git" "tmux" "systemd" "script
 do
   stow --dir="$CONFIGDIR" --target="$HOME" -vv "$project";
 done
+
+for service in nm-applet mate-settings-daemon mate-power-manager ssh-agent planck compton;
+do
+	systemctl --user enable $service
+done
+
